1b98660cd8fb51849c4f9b68b10c58806240ae3e
JoFi1977 <20jofi00@gmail.com>
2026-09-24 08:13:13 +0300

    realize class CleanUpFile

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+import os
+from types import TracebackType
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc: BaseException | None,
+        tb: TracebackType | None,
+    ) -> None:
+        if os.path.isfile(self.filename):
+            os.remove(self.filename)
